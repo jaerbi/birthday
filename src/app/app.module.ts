6ac1a8d40585821cd84524a1d0aeb6a060ae22fd
Yura Avramenko <jaerbi42@gmail.com>
2019-07-04 17:01:42 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TimeComponent } from './time/time.component';
+import { CountDownComponent } from './count-down/count-down.component';
+import { CopyRightComponent } from './copy-right/copy-right.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TimeComponent,
+    CountDownComponent,
+    CopyRightComponent
   ],
   imports: [
     BrowserModule,
